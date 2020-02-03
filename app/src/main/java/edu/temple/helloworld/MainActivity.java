c3cc536c4a9dc019b909111f9ddbcd8e3d12e05b
maxjwebber <tua83483@temple.edu>
2020-02-03 02:41:45 -0500

    implemented button message/onClick method

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,6 +3,9 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+   public void buttonMsg(View v)
+    {
+        TextView text = findViewById(R.id.message);
+        text.setText(R.string.button_alert);
+    }
+
+
 }
